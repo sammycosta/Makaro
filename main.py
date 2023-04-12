1214bbed77176f55d7b915113cb97b92fb323517
Samantha Costa <86506839+sammycosta@users.noreply.github.com>
2023-04-12 18:05:26 +0000

    Leitura arquivo em python

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Teste de uso de github online!
+
+file = open('puzzle.txt', "r")
+
+N = int(file.readline())
+
+matriz_regioes = []
+matriz_certezas = []
+for i in range(N):
+    matriz_regioes.append(file.readline().split())
+file.readline() # \n
+for i in range(N):
+    matriz_certezas.append([int(x) for x in file.readline().split()])
+
