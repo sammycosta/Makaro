51edd7cf86295e81b00bbb003b5dad53515aa793
sammycosta <arminetah@hotmail.com>
2023-04-19 09:33:26 -0300

    Primeiras funções em Python (Liveshare)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Teste de uso de github online!
-
 file = open('puzzle.txt', "r")
 
 N = int(file.readline())
@@ -8,7 +6,38 @@ matriz_regioes = []
 matriz_certezas = []
 for i in range(N):
     matriz_regioes.append(file.readline().split())
-file.readline() # \n
+file.readline()  # \n
 for i in range(N):
     matriz_certezas.append([int(x) for x in file.readline().split()])
 
+
+# solve
+
+def regiao_2_elementos(i1, j1, i2, j2):
+    if matriz_certezas[i1][j1] == 0 and matriz_certezas[i2][j2] == 0:
+        pass
+    else:
+        if matriz_certezas[i1][j1] == 1:
+            matriz_certezas[i2][j2] = 2
+        elif matriz_certezas[i1][j1] == 2:
+            matriz_certezas[i2][j2] = 1
+        elif matriz_certezas[i2][j2] == 1:
+            matriz_certezas[i1][j1] = 2
+        elif matriz_certezas[i2][j2] == 2:
+            matriz_certezas[i1][j1] = 1
+
+
+def eh_adjacente(i, j, n):
+    if i < len(matriz_certezas):
+        if matriz_certezas[i+1][j] == n:
+            return True
+    if i > 0:
+        if matriz_certezas[i-1][j] == n:
+            return True
+    if j < len(matriz_certezas[0]):
+        if matriz_certezas[i][j+1] == n:
+            return True
+    if j > 0:
+        if matriz_certezas[i][j-1] == n:
+            return True
+    return False
