638a243ea852d47784e16cac1ab10cac97c256af
gabi-FS <gabrielafdasilveira@gmail.com>
2023-05-18 16:06:49 -0300

    testando funções em lisp

--- Lisp/main.lisp
+++ Lisp/main.lisp
@@ -1,19 +1,28 @@
-(defun lerArquivo (nome_arquivo)
-  (with-open-file (stream nome_arquivo :direction :input)
-    (let ((conteudo (read stream)))
-      (format t "~a~%" conteudo)
-      conteudo)))
+;; (defun lerArquivo (nome_arquivo)
+;;   (with-open-file (stream nome_arquivo :direction :input)
+;;     (let ((conteudo (read stream)))
+;;       (format t "~a~%" conteudo)
+;;       conteudo)))
+
+;; (defun readfilee (file_name)
+;;   (with-open-file (instream file_name :direction :input)
+;;     (when instream 
+;;       (let ((string (make-string (file-length instream))))
+;;         (read-sequence string instream)
+;;         string))))
+
+(defun readFile (file_name)
+  (with-open-file (stream file_name :direction :input)
+    (loop for linha = (read-line stream nil)
+          while linha
+          collect linha)))
 
 
 
 (defun main()
-    ;; (set ((conteudo (lerArquivo "../puzzle_01.txt"))))
-    ;; (print conteudo)
-    (let ((in (open "../puzzle_01.txt")))
-        (format t "~a~%" (read-line (setq conteudo (in)))
-        (close in)))
-    (print conteudo)
-    ;; ?????????
+    (let ((content_list (readfile "../puzzle_01.txt")))
+       ((size (car content)))
+       (print size))
+    
 )
-
 (main)
