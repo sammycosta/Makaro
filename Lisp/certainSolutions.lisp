97512462b5bac183c9dec605028fcb011f6211b0
gabi-FS <gabrielafdasilveira@gmail.com>
2023-05-25 16:00:31 -0300

    iniciando módulo certainSolutions (teste aponta bugs)

---
+++ Lisp/certainSolutions.lisp
@@ -0,0 +1,59 @@
+(defpackage :CertainSolutions
+    (:use :common-lisp)
+    (:export :fillMissingOne
+                            ))
+
+(in-package :CertainSolutions)
+
+(require "Matrix" "./matrix.lisp")
+(require "PosUtils" "./positionUtils.lisp")
+(require "Validations" "./validations.lisp")
+
+
+
+;; Função auxiliar de missingNumbers que itera sobre a lista de números possíveis e sobre a lista de regiões,
+;; retornando os números ainda não utilizados em uma região e as posições não preenchidas
+(defun iterateList (possibilities empty_pos rec_list mat)
+    (let ((num (Matrix:getElement mat (car rec_list))))
+        (if (> (list-length rec_list) 0)
+            (if (/= num 0)
+                (iterateList (set-difference possibilities (list num)) empty_pos (cdr rec_list) mat)
+                (iterateList possibilities (concatenate 'list empty_pos (list (car rec_list))) (cdr rec_list) mat)
+            )
+            (list possibilities empty_pos)
+        )
+    )
+)
+
+;; Retorna os números que ainda não foram utilizados em uma região e as posições não preenchidas
+(defun missingNumbers (region_list mat)
+    (iterateList (loop for i from 1 to (PosUtils:getFirst (car region_list)) collect i)
+        '() (cdr region_list) mat)
+)
+
+;; Função auxiliar de fillMissingOne que realiza a verificação do número de posições não preenchidas na região 
+;; e realiza seu preenchimento caso haja apenas uma
+(defun fillMat1 (i missing_list mat regions)
+    (let ((aux_pos (Matrix:makePosition i 0)))
+        (if (= (PosUtils:getSecond (Matrix:getElement regions aux_pos)) 1)
+            (list (Matrix:changeElement mat (car (cadr missing_list)) (car (car missing_list))) 
+                    (Matrix:changeElement regions aux_pos (PosUtils:decreaseSecond (Matrix:getElement regions aux_pos)))
+            )
+            (list mat regions)
+        )
+    )
+)
+
+;; Chama a função fillMat1 para todas as regiões, de forma a preencher todasas regiões com apenas uma posição 
+;; não preenchida (só há uma opção para o número, nesse caso)
+(defun fillMissingOne (i mat_n_reg)
+    (let* ( (mat (car mat_n_reg))
+            (regions (cadr mat_n_reg))
+            (missing_num (fillMat1 i (missingNumbers (Matrix:getRow regions i) mat) mat regions))
+          )
+        (if (< (+ i 1) (Matrix:getRowsNumber regions))
+            (fillMissingOne (+ i 1) missing_num)
+            missing_num
+        )
+    )
+)
--- Lisp/main.lisp
+++ Lisp/main.lisp
@@ -6,6 +6,8 @@
 (require "Matrix" "./matrix.lisp")
 (require "PosUtils" "./positionUtils.lisp")
 (require "Validations" "./validations.lisp")
+(require "CertainSolutions" "./certainSolutions.lisp")
+
 ;; (ql:quickload "alexandria")
 ;; (require 'alexandria)
 
@@ -42,23 +44,28 @@
     ;; (print (Matrix:getColumnsNumber regionsMatrix 1))
     ;; (print (Matrix:getElement certaintyMatrix 0 0))
     ;; (print Matrix )
-    (let ((c certaintyMatrix)
+    (let* ((c certaintyMatrix)
         (pos (Matrix:makePosition 6 7))
-        (pos2 (Matrix:makePosition 0 4)))
+        (pos2 (Matrix:makePosition 0 4))
+        (n 7)
+        (a (loop for i from 1 to n collect i)))
         ;; (print (Matrix:changeElement c pos 88))
         ;; (print (Matrix:getElement c pos))
         ;; (Matrix:printMatrix (Matrix:addElement c pos 88))
         ;; (print (Validations:validByArrow regionsMatrix certaintyMatrix pos pos2 5))
         ;; (print (Validations:getAroundList certaintyMatrix pos (Validations:getAimed regionsMatrix pos)))
         (print (Validations:isValid certaintyMatrix regionsMatrix 1 pos2))
+        (print a)
     )
 
     
     (print (Matrix:addElement '() initial_pos 88))
     (format t "~%")
-    (Matrix:printMatrix regionsMatrix)
+    
     ;; (print (Matrix:getElement regionsMatrix initial_pos))
     (setq regions (ReadPuzzle:findRegions regionsMatrix certaintyMatrix '() initial_pos))
+    (Matrix:printMatrix certaintyMatrix)
+    (print (CertainSolutions:fillMissingOne 0 (list certaintyMatrix regions)))
     ;; (print regions)
 
  )
